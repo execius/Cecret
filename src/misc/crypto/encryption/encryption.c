86e4bf54283c9e07958186d3b60487dd1cb53294
execius <imraneqbit@proton.me>
2026-04-03 00:17:41 +0000

    made decryption counterpars of the previous encryption mechanisms

--- src/misc/crypto/encryption/encryption.c
+++ src/misc/crypto/encryption/encryption.c
@@ -305,13 +305,6 @@ cleanup:
 //                       int *outl, const unsigned char *in, int inl);
 // int EVP_EncryptFinal_ex(EVP_CIPHER_CTX *ctx, unsigned char *out, int *outl);
 //
-//
-// int EVP_DecryptInit_ex(EVP_CIPHER_CTX *ctx, const EVP_CIPHER *type,
-//                        ENGINE *impl, const unsigned char *key, const unsigned char *iv);
-// int EVP_DecryptUpdate(EVP_CIPHER_CTX *ctx, unsigned char *out,
-//                       int *outl, const unsigned char *in, int inl);
-// int EVP_DecryptFinal_ex(EVP_CIPHER_CTX *ctx, unsigned char *outm, int *outl);
-//
 int EncryptByteBuff(
             const ByteBuff_t *plain,
             const ByteBuff_t *iv,
@@ -361,7 +354,7 @@ int EncryptByteBuff(
   /*+512 just in case*/
   cipher_max = plain_size +EVP_CIPHER_get_block_size(
       encryption_options_fetchers[userconfig->encryption_option_idx]())+512;
-  MALLOC_CHECK_NULL_LOG(cipher,
+  MALLOC_CHECK_NULL_LOG(cipher_str,
       cipher_max,
       ERROR_MEMORY_ALLOCATION,
       "cannot allocate for cipher str");
@@ -426,7 +419,7 @@ int EncryptByteBuff(
       ),
       ERROR_SUCCESS,
       ERROR_BUFFINIT_FAILURE,
-      "failed to get plain  str from byte buff",
+      "failed to get init encrypted cipher byte buff",
       rc,cleanup);
 
   rc = ERROR_SUCCESS;
@@ -452,4 +445,240 @@ cleanup:
 
 
 }
+
+
+
+
+
+/*decryption stuff*/
+
+//
+// int EVP_DecryptInit_ex(EVP_CIPHER_CTX *ctx, const EVP_CIPHER *type,
+//                        ENGINE *impl, const unsigned char *key, const unsigned char *iv);
+// int EVP_DecryptUpdate(EVP_CIPHER_CTX *ctx, unsigned char *out,
+//                       int *outl, const unsigned char *in, int inl);
+// int EVP_DecryptFinal_ex(EVP_CIPHER_CTX *ctx, unsigned char *outm, int *outl);
+//
+//
+int decrypt(const EVP_CIPHER *type,
+            const unsigned char*key,
+            const unsigned char *iv,
+            const unsigned char *cipher,
+            int cipher_size,
+            unsigned char *plain,
+            int *plain_size
+            ){
+  ERROR_CHECK_NULL_LOG(type,ERROR_NULL_VALUE_GIVEN,"null value in parameter");
+  ERROR_CHECK_NULL_LOG(key,ERROR_NULL_VALUE_GIVEN,"null value in parameter");
+  ERROR_CHECK_NULL_LOG(iv,ERROR_NULL_VALUE_GIVEN,"null value in parameter");
+  ERROR_CHECK_NULL_LOG(plain,ERROR_NULL_VALUE_GIVEN,"null value in parameter");
+  ERROR_CHECK_NULL_LOG(cipher,ERROR_NULL_VALUE_GIVEN,"null value in parameter");
+  ERROR_CHECK_NULL_LOG(plain_size,ERROR_NULL_VALUE_GIVEN,"null value in parameter");
+  EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+  ERROR_CHECK_NULL_LOG(ctx,ERROR_NULL_VALUE_GIVEN,"cannot get context");
+
+  int block_size = EVP_CIPHER_block_size(type);
+  int plain_size_tmp = 0;
+  int plain_max = 0;
+  int rc = 0;
+  *plain_size = 0;
+
+  ERROR_CHECK_SUCCESS_SET_RC_GOTO_LOG(
+  (EVP_DecryptInit_ex(ctx,
+                      type,
+                      NULL,
+                      key,
+                      iv)),
+
+    LIBSSL_SUCCESS,
+    ERROR_LIBSSL_FAILURE,
+    "decrypt init failed",
+    rc,cleanup);
+
+  ERROR_CHECK_SUCCESS_SET_RC_GOTO_LOG(
+  (EVP_DecryptUpdate(ctx,
+                     plain,
+                     &plain_size_tmp,
+                     cipher,
+                     cipher_size)),
+
+    LIBSSL_SUCCESS,
+    ERROR_LIBSSL_FAILURE,
+    "decrypt update failed",
+    rc,cleanup);
+  *plain_size += plain_size_tmp;
+
+  ERROR_CHECK_SUCCESS_SET_RC_GOTO_LOG(
+  (EVP_DecryptFinal_ex(ctx,
+                     plain+*plain_size,
+                     &plain_size_tmp)),
+
+    LIBSSL_SUCCESS,
+    ERROR_LIBSSL_FAILURE,
+    "decrypt final failed",
+    rc,cleanup);
+  *plain_size += plain_size_tmp;
+
+
+  /*+512 just in case*/
+  plain_max = cipher_size - block_size+512;
+  ERROR_CHECK_SUCCESS_SET_RC_GOTO_LOG(
+  (*plain_size > plain_max),
+    1,
+    ERROR_BUF_OVERFLOW,
+    "plain buffer overflowed",
+    rc,cleanup);
+
+  rc = ERROR_SUCCESS;
+  goto cleanup;
+cleanup:
+  EVP_CIPHER_CTX_free(ctx);
+  return rc;
+
+}
+
+
+
+
+int DecryptByteBuff(
+            const ByteBuff_t *cipher,
+            const ByteBuff_t *iv,
+            ByteBuff_t **plain,
+            user_t *user)
+{
+  ERROR_CHECK_NULL_LOG(user,ERROR_NULL_VALUE_GIVEN,"null value in parameter");
+  ERROR_CHECK_NULL_LOG(iv,ERROR_NULL_VALUE_GIVEN,"null value in parameter");
+  ERROR_CHECK_NULL_LOG(plain,ERROR_NULL_VALUE_GIVEN,"null value in parameter");
+  ERROR_CHECK_NULL_LOG(cipher,ERROR_NULL_VALUE_GIVEN,"null value in parameter");
+  unsigned char *key_str = NULL;
+  unsigned char *iv_str = NULL;
+  unsigned char *plain_str = NULL;
+  unsigned char *cipher_str = NULL;
+  UserConfig_t *userconfig = NULL;
+  int plain_size = 0;
+  int cipher_size = 0;
+  int iv_size = 0;
+  int plain_max = 0;
+  int rc = 0;
+  ByteBuff_t *key = NULL;
+  ERROR_CHECK_SUCCESS_SET_RC_GOTO_LOG(
+      (UserGetUserConf(user
+                       ,&userconfig)
+      ),
+      ERROR_SUCCESS,
+      ERROR_GETUSRCONF_FAILURE,
+      "failed to get userconfig from user",
+      rc,cleanup);
+  ERROR_CHECK_SUCCESS_SET_RC_GOTO_LOG(
+      (GetLenByteBuff(cipher
+                       ,(size_t *)&cipher_size)
+      ),
+      ERROR_SUCCESS,
+      ERROR_GETLEN_FAILURE,
+      "failed to get plain len from byte buff",
+      rc,cleanup);
+  ERROR_CHECK_SUCCESS_SET_RC_GOTO_LOG(
+      (GetLenByteBuff(iv
+                       ,(size_t *)&iv_size)
+      ),
+      ERROR_SUCCESS,
+      ERROR_GETLEN_FAILURE,
+      "failed to get iv len from byte buff",
+      rc,cleanup);
+
+  /*+512 just in case*/
+  plain_max = cipher_size -EVP_CIPHER_get_block_size(
+      encryption_options_fetchers[userconfig->encryption_option_idx]())+512;
+  MALLOC_CHECK_NULL_LOG(plain_str,
+      plain_max,
+      ERROR_MEMORY_ALLOCATION,
+      "cannot allocate for plain str");
+
+
+  ERROR_CHECK_SUCCESS_SET_RC_GOTO_LOG(
+      (UserGetKey(user,
+                       &key)), 
+      ERROR_SUCCESS,
+      ERROR_USER_GET_KEY,
+      "error gettingn key from user ",
+      rc,cleanup);
+
+  ERROR_CHECK_SUCCESS_SET_RC_GOTO_LOG(
+      (GetBuffByteBuff(key
+                       ,(unsigned char **)&key_str)
+      ),
+      ERROR_SUCCESS,
+      ERROR_GETBUFF_FAILURE,
+      "failed to get key  str from byte buff",
+      rc,cleanup);
+
+  ERROR_CHECK_SUCCESS_SET_RC_GOTO_LOG(
+      (GetBuffByteBuff(iv
+                       ,(unsigned char **)&iv_str)
+      ),
+      ERROR_SUCCESS,
+      ERROR_GETBUFF_FAILURE,
+      "failed to get iv  str from byte buff",
+      rc,cleanup);
+
+  ERROR_CHECK_SUCCESS_SET_RC_GOTO_LOG(
+      (GetBuffByteBuff(cipher
+                       ,(unsigned char **)&cipher_str)
+      ),
+      ERROR_SUCCESS,
+      ERROR_GETBUFF_FAILURE,
+      "failed to get plain  str from byte buff",
+      rc,cleanup);
+
+
+
+  ERROR_CHECK_SUCCESS_SET_RC_GOTO_LOG(
+      (decrypt(encryption_options_fetchers[userconfig->encryption_option_idx]()
+               ,key_str
+               ,iv_str
+               ,cipher_str
+               ,cipher_size
+               ,plain_str
+               ,&plain_size
+      )),
+      ERROR_SUCCESS,
+      ERROR_DECRYPTION_FAILURE,
+      "failed to decrypt",
+      rc,cleanup);
+
+
+  ERROR_CHECK_SUCCESS_SET_RC_GOTO_LOG(
+      (InitByteBuff(plain
+                    ,plain_str
+                    ,(size_t)plain_size)
+      ),
+      ERROR_SUCCESS,
+      ERROR_BUFFINIT_FAILURE,
+      "failed to init byte buff for decrypted plain",
+      rc,cleanup);
+
+  rc = ERROR_SUCCESS;
+  goto cleanup;
+cleanup:
+  if (cipher_str) {
+    OPENSSL_cleanse(cipher_str,cipher_size);
+    free(cipher_str);
+  }
+  if (plain_str) {
+    OPENSSL_cleanse(plain_str,plain_size);
+    free(plain_str);
+  }
+  if (iv_str) {
+    OPENSSL_cleanse(iv_str,iv_size);
+    free(iv_str);
+  }
+  if (userconfig) {
+    OPENSSL_cleanse(userconfig,sizeof(UserConfig_t));
+    free(userconfig);
+  }
+  return rc;
+
+
+}
+
 
